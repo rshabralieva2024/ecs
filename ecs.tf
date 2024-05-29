1b19e12bef726d0b05bebcfad0af8eeec0944951
rshabralieva2024 <166457411+rshabralieva2024@users.noreply.github.com>
2024-05-29 18:44:48 -0500

    updated

---
+++ ecs.tf
@@ -0,0 +1,46 @@
+resource "aws_ecs_cluster" "foo" {
+  name = "rahat"
+
+  setting {
+    name  = "containerInsights"
+    value = "enabled"
+  }
+}
+resource "aws_ecs_task_definition" "service" {
+  family = "service"
+  container_definitions = jsonencode([
+    {
+      name      = "first"
+      image     = "rshabralieva2024/ubuntu"
+      cpu       = 10
+      memory    = 512
+      essential = true
+      portMappings = [
+        {
+          containerPort = 80
+          hostPort      = 80
+        }
+      ]
+    },
+  
+  ])
+
+  volume {
+    name      = "service-storage"
+    host_path = "/ecs/service-storage"
+  }
+
+  placement_constraints {
+    type       = "memberOf"
+    expression = "attribute:ecs.availability-zone in [us-east-2a, us-east-2b]"
+  }
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = "my-tf-test-bucket"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
